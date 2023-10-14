70e0b8e6a84fafc01164af0954258cd3788456f8
Srishti Kamra <56726634+Srishti153@users.noreply.github.com>
2023-10-14 16:28:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
-const body = document.querySelector("body");
-let css = document.querySelector("p");
+const gradientButton = document.getElementById('gradient-button');
+const box = document.getElementById("box");
+let text1 = document.getElementById("text1");
+let text2 = document.getElementById("text2");
+let code = document.getElementById("code");
 
-function changeBackground(){
-    body.style.background = "linear-gradient(to right," + input1.value + ", " + input2.value + ")";
 
-    css.textContent = body.style.background + ";";
-}
-
-input1.addEventListener("input", changeBackground);
-
-input2.addEventListener("input", changeBackground);
+gradientButton.addEventListener('click', () => {
+      const color1 = input1.value;
+      const color2 = input2.value;
+      box.style.background = "linear-gradient(to right," + input1.value + ", " + input2.value + ")";
+      text1.textContent = "Your Background is";
+      text2.textContent = "The code for the selected background is:";
+      code.textContent = box.style.background + ";";
+      box.style.display = 'block';
+});
